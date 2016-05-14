f86c209b422da80b5393480d3632c6b355cd14b9
chrom <chrom1104@gmail.com>
2016-05-15 00:18:05 +0900

    ２オクターブまで対応

--- analog_fft.ino
+++ analog_fft.ino
@@ -1,26 +1,36 @@
 #include <TimerOne.h>
 
 #define LIN_OUT 1 // wether using the linear output function or not
-#define FFT_N 128  // number of samples
+#define FFT_N 256  // number of samples
 
-#define SAMPLE_MICRO_SEC 500
+#define SAMPLE_MICRO_SEC 250
 
 // led pins
-#define PIN_DO 0
-#define PIN_RE 1
-#define PIN_MI 2
-#define PIN_FA 3
-#define PIN_SO 4
-#define PIN_RA 5
-#define PIN_SI 6
-#define PIN_DO_HIGH 7
+#define PIN_DO 2
+#define PIN_RE 3
+#define PIN_MI 4
+#define PIN_FA 5
+#define PIN_SO 6
+#define PIN_RA 7
+#define PIN_SI 8
+#define PIN_DO_HIGH 9
+#define PIN_RE_HIGH 10
+#define PIN_MI_HIGH 11
+#define PIN_FA_HIGH 12
+#define PIN_SO_HIGH 13
+// Analog 4
+#define PIN_RA_HIGH 18 
+// Analog 5
+#define PIN_SI_HIGH 19
+
+
 
 
 #include <FFT.h>
 
 void setup() {
   // put your setup code here, to run once:
-  //Serial.begin(115200);
+  Serial.begin(115200);
   Timer1.initialize(SAMPLE_MICRO_SEC); // microseconds
   Timer1.attachInterrupt(onTimer);
   Timer1.start();
@@ -34,6 +44,12 @@ void setup() {
   pinMode (PIN_RA, OUTPUT);
   pinMode (PIN_SI, OUTPUT);
   pinMode (PIN_DO_HIGH, OUTPUT);
+  pinMode (PIN_RE_HIGH, OUTPUT);
+  pinMode (PIN_MI_HIGH, OUTPUT);
+  pinMode (PIN_FA_HIGH, OUTPUT);
+  pinMode (PIN_SO_HIGH, OUTPUT);
+  pinMode (PIN_RA_HIGH, OUTPUT);
+  pinMode (PIN_SI_HIGH, OUTPUT);
 }
 
 int analogPin = 0;
@@ -60,38 +76,40 @@ void flush_led (uint16_t *fft) {
 ド：33, 34
 */
   // 各配列の全てがthresholdを超えていたらそれと判断する
-  char onkai_freq[][3] = {
-    {16, 17, 18}, // ド
+  static const char onkai_freq[][3] = {
+    {16, 17, -1}, // ド
     {18, 19, -1}, // レ
     {20, 21, 22}, // ミ
-    {21, 22, 23}, // ファ
-    {24, 25, 26}, // ソ
-    {27, 28, 29}, // ラ
-    {31, 32, 33}, // シ
-    {33, 34, -1}, // ド
+    {-1, 22, 23}, // ファ
+    {-1, 25, 26}, // ソ
+    {-1, 28, 29}, // ラ
+    {31, 32, -1}, // シ
+    {33, 34, -1}, // ド HIGH
+    {37, 38, -1}, // レ HIGH
+    {-1, 42, 43}, // ミ HIGH
+    {44, 45, -1}, // ファ HIGH
+    {-1, 50, 51}, // ソ HIGH
+    {-1, 56, -1}, // ラ HIGH 
+    {63, 64, -1}, // シ HIGH
   };
-  char pins[] = {
-    PIN_DO, PIN_RE, PIN_MI, PIN_FA, PIN_SO, PIN_RA, PIN_SI, PIN_DO_HIGH,
+  static const char pins[] = {
+    PIN_DO,       PIN_RE,       PIN_MI,       PIN_FA,       PIN_SO,       PIN_RA,       PIN_SI,
+    PIN_DO_HIGH,  PIN_RE_HIGH,  PIN_MI_HIGH,  PIN_FA_HIGH,  PIN_SO_HIGH,  PIN_RA_HIGH,  PIN_SI_HIGH,
   };
-  int threshold = 10;
+  int threshold = 7;
   int i;
   for (i = 0 ; i < sizeof(onkai_freq) / sizeof(onkai_freq[0]) ; i++) {
     int j;
-    int ok  = 1;
+    int output  = HIGH;
     for (j = 0 ; j < sizeof(onkai_freq[i]) / sizeof(onkai_freq[i][0]) ; j++) {
       if (onkai_freq[i][j] == -1)  continue;
       if (fft[onkai_freq[i][j]] < threshold) {
         // 超えていない = NG
-        ok  = 0;
+        output  = LOW;
         break;
       }
     }
-    if (ok) {
-      digitalWrite(pins[i], HIGH);
-    }
-    else {
-      digitalWrite(pins[i], LOW);
-    }
+    digitalWrite(pins[i], output);
   }
 }
 
@@ -105,7 +123,7 @@ void loop() {
     fft_idx = 0;
     return;
   } else {
-    // 1秒間に5回表示したい
+    // 1秒間に表示する回数を抑える
     bunshu = (float)1000 * 1000 / (float)(SAMPLE_MICRO_SEC * FFT_N) / 5 - 1;
   }
 
@@ -117,7 +135,7 @@ void loop() {
   fft_run();
   fft_mag_lin();
 
-/*
+//*
   for (int i = 0 ; i < FFT_N / 2 ; i++) {    
     if (fft_lin_out[i] < 10)  continue;
     Serial.print("[idx: ");
@@ -127,7 +145,7 @@ void loop() {
     Serial.print("]  ");
   }
   Serial.println();
-*/
+//*/
   flush_led(fft_lin_out);
   fft_idx = 0;
   interrupts();
